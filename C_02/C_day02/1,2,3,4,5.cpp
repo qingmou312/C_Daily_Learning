235c6f070a77316f0a4357f74d52e8eae6fdc74d
qingmou <Lidanzing@163.com>
2018-12-16 22:51:37 +0800

    提交代码

---
+++ C_02/C_day02/1,2,3,4,5.cpp
@@ -0,0 +1,100 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 4;
+//	int j = 12;
+//	int k = 0;
+//	k = i;
+//	i = j;
+//	j = k;
+//	printf("i = %d j = %d\n", i, j);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 4;
+//	int j = 12;
+//	printf("i = %d j = %d\n", j, i);
+//	return 0;
+//}
+//
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	int a[] = { 1,3,2,5,4,6,7,5,3,9 };
+//	int max = a[0];
+//	int sz = sizeof(a)/ sizeof(a[0]);
+//	for (i = 1; i < sz; i++)
+//	{
+//		if (max < a[i])
+//		{
+//			 max = a[i];
+//		}
+//		
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int i = 5;
+//	int j = 7;
+//	int k = 4;
+//	int tmp = 0;
+//	if (i < j)
+//	{
+//		tmp = i;
+//		i = j;
+//		j = tmp;
+//	}
+//	if (i < k)
+//	{
+//		tmp = i;
+//		i = k;
+//		k = i;
+//	}
+//	if (j < k)
+//	{
+//		tmp = j;
+//		j = k;
+//		k = j;
+//	}
+//	printf("%d>%d>%d", i, j, k);
+//	return 0;
+//}
+//
+//
+//
+//int main()
+//{
+//	int i = 25;
+//	int j = 100;
+//
+//
+//	return 0;
+//}
+//
+//
+// 
+// int main()
+// {
+// 	int i = 25;
+// 	int j = 100;
+// 	int k = 0;
+// 	while(k=i%j)
+// 	{
+// 		i = j;
+// 		j = k;
+// 	}
+// 	printf("%d\n", j);
+// 	return 0;
+// }
+//
+//
+//
+//
